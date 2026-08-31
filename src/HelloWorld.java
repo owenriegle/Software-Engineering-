c4b204286196e1f3268b0481ddcaef07db349fb9
Owen A. Riegle <riegleowen@outlook.com>
2026-08-31 16:13:17 -0400

    fix helloworld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
 public class HelloWorld {
-	System.out.println("Hello world!")
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
 }
